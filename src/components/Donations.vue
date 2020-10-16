4cec190e04b514c625eb1303a23f359e53693547
Aeval <ankouaeval@gmail.com>
2020-10-16 05:03:33 -0400

    fix the firestore security

--- src/components/Donations.vue
+++ src/components/Donations.vue
@@ -1,105 +1,84 @@
 <template>
-  <div>
-    <ul class="donations-list">
-      <li
-        class="container mt-2"
-        v-bind:key="donation.id"
-        v-for="donation in donations"
-      >
-        <div
-          v-bind:style="[
-            donation.donation_amount > 100
-              ? { 'background-color': '#D4AF37' }
-              : { 'background-color': '#42b983' },
-          ]"
-          class="donation-row row mx-auto my-1 rounded hover"
-        >
-          <div class="col-2 my-2 text-left">
-            <img
-              src="../assets/party-popper.png"
-              alt="party icon"
-              height="50px"
-              width="50px"
-            />
-          </div>
-          <p class="col my-auto">{{ donation.donation_name }}</p>
-          <p class="col my-auto">${{ donation.donation_amount }}</p>
-        </div>
-      </li>
-    </ul>
-  </div>
+	<div>
+		<ul class="donations-list">
+			<li class="container mt-2" v-bind:key="donation.id" v-for="donation in donations">
+				<div v-bind:style="[donation.donation_amount > 100 ? { 'background-color': '#D4AF37' } : { 'background-color': '#42b983' }]" class="donation-row row mx-auto my-1 rounded hover">
+					<div class="col-2 my-2 text-left">
+						<img src="../assets/party-popper.png" alt="party icon" height="50px" width="50px" />
+					</div>
+					<p class="col my-auto">{{ donation.donation_name }}</p>
+					<p class="col my-auto">${{ donation.donation_amount }}</p>
+				</div>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import firebase from "firebase";
+	import firebase from 'firebase';
 
-export const db = firebase
-  .initializeApp({ projectId: "vue-donation" })
-  .firestore();
+	export const db = firebase.initializeApp({ projectId: 'vue-donation', apiKey: ' AIzaSyAVJwGwkiy0lStur9TgIqA2GxgR-VuixQs' }).firestore();
 
-export default {
-  name: "Donations",
-  data() {
-    return {
-      donations: [],
-    };
-  },
-  firebase: {
-    donations: db.collection("donations"),
-  },
-  created() {
-    this.$bind(
-      "donations",
-      db.collection("donations").orderBy("created_at", "desc")
-    )
-      .then((donations) => (this.donations = donations))
-      .catch((err) => console.log(err));
-  },
-};
+	export default {
+		name: 'Donations',
+		data() {
+			return {
+				donations: [],
+			};
+		},
+		firebase: {
+			donations: db.collection('donations'),
+		},
+		created() {
+			this.$bind('donations', db.collection('donations').orderBy('created_at', 'desc'))
+				.then(donations => (this.donations = donations))
+				.catch(err => console.log(err));
+		},
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	h3 {
+		margin: 40px 0 0;
+	}
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+	a {
+		color: #42b983;
+	}
 
-.donation-row {
-  background-color: #42b983;
-  width: 75%;
-  box-shadow: rgba(70, 70, 70, 0.933) 2px 2px 1px;
-}
+	.donation-row {
+		background-color: #42b983;
+		width: 75%;
+		box-shadow: rgba(70, 70, 70, 0.933) 2px 2px 1px;
+	}
 
-.donation-row:hover {
-  box-shadow: rgba(54, 54, 54, 0.933) 5px 5px 5px;
-  background-color: #4cca92;
-}
+	.donation-row:hover {
+		box-shadow: rgba(54, 54, 54, 0.933) 5px 5px 5px;
+		background-color: #4cca92;
+	}
 
-p {
-  color: aliceblue;
-  font-size: 1.5em;
-}
+	p {
+		color: aliceblue;
+		font-size: 1.5em;
+	}
 
-.donations-list {
-  overflow-y: scroll;
-  overflow-x: hidden;
-  height: 350px;
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
-}
+	.donations-list {
+		overflow-y: scroll;
+		overflow-x: hidden;
+		height: 350px;
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+	}
 
-.donations-list::-webkit-scrollbar {
-  display: none;
-}
+	.donations-list::-webkit-scrollbar {
+		display: none;
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,23 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import { firestorePlugin } from 'vuefire'
-import firebase from 'firebase'
+import { firestorePlugin } from 'vuefire';
+import firebase from 'firebase';
 
-const { Timestamp } = firebase.firestore
-export { Timestamp }
+const { Timestamp } = firebase.firestore;
+export { Timestamp };
 
-Vue.use(firestorePlugin)
-Vue.use(firebase)
+Vue.use(firestorePlugin);
+Vue.use(firebase, { apiKey: ' AIzaSyAVJwGwkiy0lStur9TgIqA2GxgR-VuixQs' });
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
-
-
-
+	router,
+	render: h => h(App),
+}).$mount('#app');
